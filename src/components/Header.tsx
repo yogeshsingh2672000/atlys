6e1a248526fee7456c0be3926e0bc5d215a7532a
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2025-10-15 22:18:57 +0530

    header fix: login conditional render on authenticcation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useAuthGate } from '@components/AuthGate'
 
 type Props = {
   fixed?: boolean 
 }
 
 export default function Header({ fixed = false }: Props) {
+  const { isAuthed } = useAuthGate()
   const positionClass = fixed ? 'fixed' : 'sticky'
   return (
     <>
@@ -13,7 +16,9 @@ export default function Header({ fixed = false }: Props) {
       >
         <div className="mx-auto max-w-6xl h-full px-4 flex items-center justify-between">
           <div className='text-xl font-bold'>Foo-rum</div>
-          <div className='text-xl font-bold'>Login</div>
+          {!isAuthed && (
+            <Link to="/login" className='text-sm font-medium text-indigo-600 hover:underline'>Login</Link>
+          )}
         </div>
       </header>
       {fixed && <div className="h-[80px]" />}
